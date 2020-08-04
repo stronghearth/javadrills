3ab7c388cff622121986becfeab260bff49eed70
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-08-04 17:08:40 -0400

    wrote out string variables and system print each and created class file

--- simple_string_formatting/com/thinkful/drill/SimpleStringFormatting.java
+++ simple_string_formatting/com/thinkful/drill/SimpleStringFormatting.java
@@ -2,6 +2,34 @@ package com.thinkful.drill;
 
 public class SimpleStringFormatting {
     public static void main (String[] args) {
+        String movieOne = "1. Star Wars Episode V: Empire Strikes Back";
+        System.out.println(movieOne);
 
+        String castOne = "Mark Hamill, Harrison Ford";
+        System.out.println(castOne);
+
+        String movieTwo = "2. Princess Mononoke";
+        System.out.println(movieTwo);
+
+        String castTwo = "Billy Bob Thornton, Minnie Driver";
+        System.out.println(castTwo);
+
+        String movieThree = "3. Thor: Ragnarok";
+        System.out.println(movieThree);
+
+        String castThree = "Chris Hemsworth, Tom Hiddleston";
+        System.out.println(castThree);
+
+        String movieFour = "4. Stardust";
+        System.out.println(movieFour);
+
+        String castFour = "Charlie Cox, Claire Daines";
+        System.out.println(castFour);
+
+        String movieFive = "5. Doctor Strange";
+        System.out.println(movieFive);
+
+        String castFive = "Benedict Cumberbatch, Rachel McAdams";
+        System.out.println(castFive);
     }
 }

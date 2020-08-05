9778dbc85c0c19ad77e7009fa6b10869d2c7f717
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-08-05 11:34:08 -0400

    implemented line breaks and appended rest of receipt strings

--- receipt_printer/com/thinkful/receipt_printer/ReceiptPrinter.java
+++ receipt_printer/com/thinkful/receipt_printer/ReceiptPrinter.java
@@ -7,10 +7,28 @@ public class ReceiptPrinter {
 
         String dashes = "------------------";
 
+        String lineBreak = System.getProperty("line.separator");
 
         sb.append("Bill");
+        sb.append(lineBreak);
         sb.append(dashes);
+        sb.append(lineBreak);
+        sb.append("Pizza       $12.99");
+        sb.append(lineBreak);
+        sb.append("Soda         $1.25");
+        sb.append(lineBreak);
+        sb.append("Cookies      $2.50");
+        sb.append(lineBreak);
+        sb.append("SubTotal:   $16.74");
+        sb.append(lineBreak);
+        sb.append("Tax(10%):    $1.67");
+        sb.append(lineBreak);
+        sb.append("Service(15%):$2.51");
+        sb.append(lineBreak);
         sb.append(dashes);
+        sb.append(lineBreak);
+        sb.append("Total:      $20.92");
+        sb.append(lineBreak);
         sb.append(dashes);
 
         System.out.println(sb);

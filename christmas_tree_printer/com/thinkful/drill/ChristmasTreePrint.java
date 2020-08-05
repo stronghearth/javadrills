eb41cf05a721ddea0ef3e35d583b8e6f314d3fad
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-08-05 11:49:01 -0400

    created string and concatenated the different pieces of tree

---
+++ christmas_tree_printer/com/thinkful/drill/ChristmasTreePrint.java
@@ -0,0 +1,24 @@
+package com.thinkful.drill;
+
+public class ChristmasTreePrint {
+    public static void main(String[] args) {
+        String tree = "        *        \n" +
+        "       ***       \n" +
+        "      *****      \n" +
+        "       ***       \n" +
+        "      *****      \n" +
+        "       ***       \n" +
+        "      *****      \n" +
+        "     *******     \n" +
+        "    *********    \n" +
+        "     *******     \n" +
+        "    *********    \n" +
+        "   ***********   \n" +
+        "  *************  \n" +
+        "       ***       \n" +
+        "       ***       \n" +
+        "       ***       \n";
+
+        System.out.println(tree);
+    }
+}

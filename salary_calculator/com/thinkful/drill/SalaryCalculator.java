fc8bcdcaccb1ae4be45c859711a44c15ad3e94b7
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-08-17 15:47:33 -0400

    adjusted to accomodate for doubles

--- salary_calculator/com/thinkful/drill/SalaryCalculator.java
+++ salary_calculator/com/thinkful/drill/SalaryCalculator.java
@@ -1,22 +1,22 @@
 package com.thinkful.drill;
-import java.util.scanner;
+import java.util.Scanner;
 
 public class SalaryCalculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         //Ask user for a String value
-        System.out.println("Please provide your hourly rate (ie $40.00): ");
+        System.out.println("Please provide your hourly rate (ie 40.00): ");
         
-        double rate = scanner.next();
+        double rate = scanner.nextDouble();
     
         
     
-        System.out.println("Please provide the number of hours worked (ie 20)");
+        System.out.println("Please provide the number of hours worked (ie 20): ");
         // read the age
-        int hours = scanner.nextInt();
+        double hours = scanner.nextDouble();
 
-        int total = rate * hours;
+        
         //print the age
-        System.out.printf("The total pay is ", total);
+        System.out.printf("The total pay is %f per week", rate * hours);
     }
 }

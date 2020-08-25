7723bd8da65ac44aa6e9368d39c71dbd32410295
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-08-25 09:45:16 -0400

    wrote file structure

---
+++ as_the_crow_flies/com/thinkful/drill/CrowFlies.java
@@ -0,0 +1,10 @@
+package com.thinkful.drill;
+import java.util.Scanner;
+
+public class CrowFlies {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}

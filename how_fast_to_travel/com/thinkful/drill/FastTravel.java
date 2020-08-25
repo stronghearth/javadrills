77aea05b9c22f7209d25289e67c9c3145297d85b
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-08-25 10:21:24 -0400

    wrote FastTravel class

--- how_fast_to_travel/com/thinkful/drill/FastTravel.java
+++ how_fast_to_travel/com/thinkful/drill/FastTravel.java
@@ -5,6 +5,12 @@ public class FastTravel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.println("Please enter the distance to your destination: ");
+        double distance = scanner.nextDouble();
+
+        System.out.println("Please enter the length of time you have to get there: ");
+        double time = scanner.nextDouble();
+
+        System.out.printf("%f", distance/time);
     }
 }

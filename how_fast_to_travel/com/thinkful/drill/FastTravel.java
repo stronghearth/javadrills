72dd6cf3d7486d049ddecaa36c7e62fc9c9291f8
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-08-25 10:17:27 -0400

    set up file structure

---
+++ how_fast_to_travel/com/thinkful/drill/FastTravel.java
@@ -0,0 +1,10 @@
+package com.thinkful.drill;
+import java.util.Scanner;
+
+public class FastTravel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
